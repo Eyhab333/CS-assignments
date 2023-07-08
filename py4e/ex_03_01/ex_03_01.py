94f928ee73188e508c3495a190fb6b3cd4e6850e
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-07-08 23:26:11 +0300

    ex3

---
+++ py4e/ex_03_01/ex_03_01.py
@@ -0,0 +1,13 @@
+string_hours = input("Enter Hours: ")
+string_rate = input("Enter Rate: ")
+
+float_hours = float(string_hours)
+float_rate = float(string_rate)
+
+if float_hours > 40:
+    regular_pay = float_hours * float_rate
+    over_time_pay = (float_hours - 40.0) * (float_rate * 0.5)
+    extra_pay = regular_pay + over_time_pay
+else:
+    extra_pay = float_hours * float_rate
+print("Pay:", extra_pay)
