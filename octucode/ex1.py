f0d022d39449b17eec9b8619779d271d30960301
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-07-09 22:39:53 +0300

    octucode exercise

---
+++ octucode/ex1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Youtube Channel Name Generator:")
+user_nick_name = input("What is your nick name? \n")
+user_channel_topic = input("What is your chaneel about? \n")
+generated_channel_name = user_channel_topic + " with " + user_nick_name
+print("You could name your channel " + "(" + generated_channel_name + ")")
