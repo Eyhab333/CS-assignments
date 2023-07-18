93c703f57fc3333193fd706f3f34b7166161c404
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-07-18 22:32:49 +0300

    ex_7_1

---
+++ py4e/ex_07/ex_07.py
@@ -0,0 +1,5 @@
+# Exercise 1: Write a program to read through a file and print 
+# the contents of the file (line by line) all in upper case.
+
+file_handling = open('nbox_short.txt')
+print(file_handling)
