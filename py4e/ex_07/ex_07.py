b5834ad8f9a3d4bf3701892a8c0fd7f2072c8d07
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-07-19 22:11:12 +0300

    ex 7

--- py4e/ex_07/ex_07.py
+++ py4e/ex_07/ex_07.py
@@ -1,5 +1,7 @@
 # Exercise 1: Write a program to read through a file and print 
 # the contents of the file (line by line) all in upper case.
 
-file_handling = open('nbox_short.txt')
-print(file_handling)
+file_handling = open("C:\\Users\\1\\Desktop\\CS-assignments\\py4e\\ex_07\\mbox_short.txt", "rt")
+for line in file_handling:
+    lines_without_new_line = line.rstrip()
+    print(lines_without_new_line.upper())
