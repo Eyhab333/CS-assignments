d73d2360aaf5c1567c8838c301f4ac1e1eb6f22a
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-07-22 20:07:02 +0300

    var

--- octucode/ex_2.py
+++ octucode/ex_2.py
@@ -1,4 +1,5 @@
 numbers = input('Please enter two digits: \n')
 first_number = int(numbers[0])
 second_number = int(numbers[1])
-print(first_number + second_number)
+sum = first_number + second_number
+print(sum)
