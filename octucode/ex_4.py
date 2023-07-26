e503cf8088720fcc1bd0b26f6ea002bb04b81003
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-07-26 19:30:11 +0300

    ex4

---
+++ octucode/ex_4.py
@@ -0,0 +1,6 @@
+total_minutes = input("Please type the number of minutes:\n")
+int_minutes = int(total_minutes)
+hours = int_minutes // 60
+minutes = int_minutes % 60
+print("This course is: " + str(hours) + " hours and " + str(minutes) +
+      " minutes long")
