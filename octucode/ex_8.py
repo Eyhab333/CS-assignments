62a2309ded40dcbfc9f132553d18034f8a624d11
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-08-01 22:04:04 +0300

    ex_8

---
+++ octucode/ex_8.py
@@ -0,0 +1,5 @@
+seconds = int(input("Enter the duration in seconds:\n"))
+hours = seconds // 3600
+minutes = (seconds % 3600) // 60
+remaining_seconds = seconds % 60
+print(f"The duration is: {hours} hours, {minutes} mintes and {remaining_seconds} seconds")
