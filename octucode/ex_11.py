0c76509ff73038c04bfebd0c4b26dd088ae84b7f
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-08-09 16:43:12 +0300

    ex11

---
+++ octucode/ex_11.py
@@ -0,0 +1,10 @@
+# Student grading software
+number = float(input("Enter your grade:\n"))
+if number >= 90:
+    print("Excellence")
+elif number >= 75:
+    print("good")    
+elif number >= 50:
+    print("acceptable")    
+else:
+    print("weak")    
