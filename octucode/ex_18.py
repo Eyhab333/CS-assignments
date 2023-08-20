f541348248ffd48886e6576e9a0a8668ef09941c
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-08-20 22:48:06 +0300

    ex_18

---
+++ octucode/ex_18.py
@@ -0,0 +1,40 @@
+print("""
+   _______________                        |*\_/*|________
+
+  |  ___________  |     .-.     .-.      ||_/-\_|______  |
+
+  | |           | |    .****. .****.     | |           | |
+
+  | |   0   0   | |    .*****.*****.     | |   0   0   | |
+
+  | |     -     | |     .*********.      | |     -     | |
+
+  | |   \___/   | |      .*******.       | |   \___/   | |
+
+  | |___     ___| |       .*****.        | |___________| |
+
+  |_____|\_/|_____|        .***.         |_______________|
+
+    _|__|/ \|_|_.............*.............._|________|_
+
+   / ********** \                          / ********** \
+
+ /  ************  \                      /  ************  \
+
+--------------------                    --------------------
+
+      """)
+
+print("""
+Welcome to my island!
+There are two doors in front of you, a 🆁 🅴 🅳  🅳 🅾 🅾 🆁 and a 🅱 🅻 🆄 🅴  🅳 🅾 🅾 🆁""")     
+
+user_choise = input("Which door do you want to open? enter blue or red\n").lower()
+if user_choise == "red":
+    print("dddddd")
+elif user_choise == "blue":
+    print("hhhhhhh")
+else:
+    print("not a valid entry, please just write 'blue' or 'red'")
+    print("Game over")        
+
