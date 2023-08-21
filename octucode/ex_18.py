029e6df4260667eaf4c8331b2567fc2ce4b78906
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-08-21 18:31:15 +0300

    update

--- octucode/ex_18.py
+++ octucode/ex_18.py
@@ -29,11 +29,13 @@ print("""
 Welcome to my island!
 There are two doors in front of you, a 🆁 🅴 🅳  🅳 🅾 🅾 🆁 and a 🅱 🅻 🆄 🅴  🅳 🅾 🅾 🆁""")     
 
-user_choise = input("Which door do you want to open? enter blue or red\n").lower()
-if user_choise == "red":
+user_choise_doors = input("Which door do you want to open? enter blue or red\n").lower()
+if user_choise_doors == "red":
     print("dddddd")
-elif user_choise == "blue":
+    
+elif user_choise_doors == "blue":
     print("hhhhhhh")
+    user_choise_boxes = input("Now, You have to choose between three boxes\n")
 else:
     print("not a valid entry, please just write 'blue' or 'red'")
     print("Game over")        
