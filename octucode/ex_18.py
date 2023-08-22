1dc19ae9f143bb797a4f42393ea83ff1692bac40
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-08-22 19:04:53 +0300

    ex18

--- octucode/ex_18.py
+++ octucode/ex_18.py
@@ -31,11 +31,22 @@ There are two doors in front of you, a 🆁 🅴 🅳  🅳 🅾 🅾 🆁 and a
 
 user_choise_doors = input("Which door do you want to open? enter blue or red\n").lower()
 if user_choise_doors == "red":
-    print("dddddd")
-    
+    print("bad")
+    print('Game over')
 elif user_choise_doors == "blue":
-    print("hhhhhhh")
-    user_choise_boxes = input("Now, You have to choose between three boxes\n")
+    print("nice")
+    user_choise_boxes = input("Now, You have to choose between three boxes! first - second -third\n").lower()
+    if user_choise_boxes == 'first':
+        print("first nice")
+    elif user_choise_boxes == 'second':
+        print("second bad")
+        print('Game over')
+    elif user_choise_boxes == 'third':
+        print('third very bad')
+        print('Game over')
+    else:
+        print('invalid entry')            
+
 else:
     print("not a valid entry, please just write 'blue' or 'red'")
     print("Game over")        
