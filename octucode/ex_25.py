61ff9e2790f19fc07d04fb479d19a0a60b9e8c2e
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-09-25 21:58:07 +0300

    update

--- octucode/ex_25.py
+++ octucode/ex_25.py
@@ -1,5 +1,18 @@
 library = []
-user_book_one =  input("Enter the name of a book you own:")
+user_book_one =  input("Enter the name of a book you own: ")
 user_book2_maybe =  input("Enter the name of another book you own (or press'Enter' to skip): ")
-library = user_book2_maybe + user_book_one
+library.append(user_book_one)
+library.append(user_book2_maybe)
 print(f"Your Library: {library}")
+
+# Enter the name of a book you wish ti have in the future: 
+# Enter the name of another book you wish to have (or press'Enter' to skip):
+
+#Enter the name of a book from your wishlist that you've acquired  (or press'Enter' to skip):
+
+# Updated Library: 
+# Updated Wishlist:
+
+# Enter the name of a book from your library you wish to donate (or press'Enter' to skip): 
+
+# Final Library after Donations: 
