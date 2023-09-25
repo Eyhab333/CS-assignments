79be774fff2ac7750f482be3086dedaf3f65a77d
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-09-25 21:10:37 +0300

    ex25

--- octucode/ex_25.py
+++ octucode/ex_25.py
@@ -1,1 +1,5 @@
-input("Enter the name of a boo")
+library = []
+user_book_one =  input("Enter the name of a book you own:")
+user_book2_maybe =  input("Enter the name of another book you own (or press'Enter' to skip): ")
+library = user_book2_maybe + user_book_one
+print(f"Your Library: {library}")
